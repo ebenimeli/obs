99d0e73f5ea07e93ce76bbc59b0b5164be614951
Enrique Benimeli <ebenimeli@gmail.com>
2023-03-11 17:38:20 +0100

    Minor fix: value text in checkboxes

--- php/subjects/checkboxes.php
+++ php/subjects/checkboxes.php
@@ -25,6 +25,7 @@
                 $j = $i;
                 for ($i = $j; $i < ($vsize/2); $i++) {
                     $theobs = $ot[$i];
+                    $theobs = rtrim(rtrim(rtrim($theobs, "\n"), "\r\n"), "\r");
 
                     echo "<input type='checkbox' value='$theobs' onchange='updateText()'>" . $o[$i];
                     echo "<br/>";
